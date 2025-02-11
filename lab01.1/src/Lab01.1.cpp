bcdd4eea19e845f4641bf8c3b368d3b0e7f98adb
user <khavich2007@gmail.com>
2025-02-11 15:19:15 +0200

    Init

---
+++ lab01.1/src/Lab01.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "Lab01.1.hpp"
+
+using std::cout;
+using std::endl;
+using std::pow;
+using std::exp;
+using std::cos;
+
+int main()
+{
+	cout << "==================" << endl;
+	cout << "\t Lab01.1 \t" << endl;
+	cout << "==================" << endl;
+	double a = 2.61, b = 3.43, x = 5.54;
+	double y = pow(2 - x, 1.0 / 4);
+	double z = b * log(2 * x);
+	auto r = pow(a, y) + exp(z) + cos(a * x);
+	cout << "==================" << endl;
+	cout << "Result" << r << endl;
+	cout << "==================" << endl;
+	return 0;
+}
