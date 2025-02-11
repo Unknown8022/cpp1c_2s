27c548cd73441828b4499f6584946f9ecb8f9dac
user <khavich2007@gmail.com>
2025-02-11 15:30:48 +0200

    lab01.2

---
+++ lab01.2/src/LAB01.2.cpp
@@ -0,0 +1,25 @@
+﻿#include "LAB01.2.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::pow;
+using std::exp;
+using std::cos;
+
+int main()
+{
+	cout << "==================" << endl;
+	cout << "\t LAB01.2 \t" << endl;
+	cout << "==================" << endl;
+	double a, b, x;
+	cout << "Enter a, b, x" << endl;
+	cin >> a >> b >> x;
+	double y = pow(2 - x, 1.0 / 4);
+	double z = b * log(2 * x);
+	auto r = pow(a, y) + exp(z) + cos(a * x);
+	cout << "==================" << endl;
+	cout << "Result" << r << endl;
+	cout << "==================" << endl;
+	return 0;
+}
